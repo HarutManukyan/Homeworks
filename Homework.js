c1973011c469824fb46c37b30033f4f6b84c7d1b
HarutManukyan <102929456+HarutManukyan@users.noreply.github.com>
2022-05-02 13:46:30 +0400

    Add files via upload

--- Homework.js
+++ Homework.js
@@ -1,37 +1,27 @@
-let implementMap = (arr, map) => {
-    let action = prompt()
-    switch(action){
-      case 'change':
-        for(let i = 0; i < arr.length; i++){
-          arr[i] = map 
-       }
-       break;
-       case '*':
-        for(let i = 0; i < arr.length; i++){
-          arr[i] =arr[i] * map 
-       }
-       break;
-       case '/':
-        for(let i = 0; i < arr.length; i++){
-          arr[i] /= map 
-       }
-       break;
-       case '+':
-        for(let i = 0; i < arr.length; i++){
-          arr[i] += map 
-       }
-       break;
-       case '-':
-        for(let i = 0; i < arr.length; i++){
-          arr[i] = arr[i] - map 
-       }
-       break;
-    }
-    if(a = 'change'){
-      
-    }
-    
-    return arr
-    
-  }
-  console.log(implementMap([1, 2, 3, 4, 5, 6, 7], 5))
+
+
+//1. Given an array. Write a recursive function that removes the first element and returns the given
+//array. (without using arr.unshift(),assign second element to first, third element to second...)
+let removeFirst = arr => {
+ let arr1 = arr.splice(0, 1)
+ return arr
+}
+console.log(removeFirst([6, 78, 'n', 0, 1]))
+
+
+
+//3. Given an array and a number N.  Write a recursive function that rotates an array N places to the
+//left. (Hint: to add element to the beginning use arr.unshift())
+let rotateArr = (arr, num, n = 0) => {
+if(n !== num){
+  let pop = arr.pop()
+  let shift = arr.unshift(pop)
+} else {
+  return arr
+}
+return rotateArr(arr,num, n + 1)
+
+}
+console.log(rotateArr([1, 2, 3, 4, 5], 2))
+
+
