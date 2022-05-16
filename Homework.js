62dabdc30c79f4ba9b42995ef17d357504dfe9ac
HarutManukyan <102929456+HarutManukyan@users.noreply.github.com>
2022-05-16 23:11:05 +0400

    Add files via upload

--- Homework.js
+++ Homework.js
@@ -1,27 +1,34 @@
+//3. Write a recursive function to determine whether all digits of the number are odd or
+//not.
+let checkOdd = (num) => {
+        
+  let lastDigit = num % 10
+  if(lastDigit % 2 == 0){
 
-
-//1. Given an array. Write a recursive function that removes the first element and returns the given
-//array. (without using arr.unshift(),assign second element to first, third element to second...)
-let removeFirst = arr => {
- let arr1 = arr.splice(0, 1)
- return arr
+      return false
+  } else {
+      num = (num - lastDigit)/10
+  }
+  checkOdd(num)
 }
-console.log(removeFirst([6, 78, 'n', 0, 1]))
+console.log(checkOdd(1211133))
 
 
 
-//3. Given an array and a number N.  Write a recursive function that rotates an array N places to the
-//left. (Hint: to add element to the beginning use arr.unshift())
-let rotateArr = (arr, num, n = 0) => {
-if(n !== num){
-  let lastElement = arr.pop()
-  arr.unshift(lastElement)
-} else {
-  return arr
+
+
+//4. Given an array of numbers. Write a recursive function to find its //minimal positive element. (if
+//such element does not exist, return -1)․
+let findMinPos = (arr, n = 0) => {
+  arr.sort()
+  if(arr[0] < 0){
+      arr.splice(0, 1)
+  } else {
+      return arr[0]
+  }
+  findMinPos(arr)
 }
-return rotateArr(arr,num, n + 1)
-
-}
-console.log(rotateArr([1, 2, 3, 4, 5], 2))
+console.log(findMinPos([56, -9, 87, -23, 0, -105, 55, 1]))
 
 
+//Չեմ հասկանում խի undefined-են տալի
